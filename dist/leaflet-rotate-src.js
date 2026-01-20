2f1e178a4c64591d7496aa2daa17c9e6ff4b01b1
Yao Lu <yaolu@transfon.com>
2026-01-20 16:34:41 +0800

    update dist

--- dist/leaflet-rotate-src.js
+++ dist/leaflet-rotate-src.js
@@ -1806,7 +1806,7 @@
         link.title = 'Rotate map';
 
         L.DomEvent.on(link, 'dblclick', L.DomEvent.stopPropagation)
-          .on(link, 'mousedown', this._handleMouseDown, this)
+          // .on(link, 'mousedown', this._handleMouseDown, this)
           .on(link, 'click', L.DomEvent.stop)
           .on(link, 'click', this._cycleState, this)
           .on(link, 'click', this._refocusOnMap, this);
